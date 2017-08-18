734dc042398a2c8301d4f847c970e71f72d88044
Chris Dinehart <chris.dinehart@northstaralarm.com>
2017-08-18 15:50:14 -0600

    small git notes revision

--- Git.js
+++ Git.js
@@ -4,8 +4,8 @@ ls -A // Show everything, including hidden files.
 
 Git init // Create a git repository.
 Git status // Shows status of repository files.
-Git remote add origin [repository link] // Connect the repository you want to save to.
+    Git remote add origin [repository link] // Connect the repository you want to save to.
 Git add 'name of file'// Stage's the file to be added.
-Git commit -M "detailed description of commit"// Commit's file.
+Git commit -m "detailed description of commit"// Commit's file.
 Git push origin master // Pushes file to GitHub.
 Get out of VIM: esc, :q // Get out of anything in the terminal safely.
